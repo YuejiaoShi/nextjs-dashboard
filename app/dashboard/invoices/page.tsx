b76c5dc4b823f83846e1c46982b6c04931547000
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-31 20:56:50 +0200

    remove logs, and add a log to see the term changing

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -16,7 +16,7 @@ export default async function Page({
 }) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
-  console.log(query, currentPage);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
