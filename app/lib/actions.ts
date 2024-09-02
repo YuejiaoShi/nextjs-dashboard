3c39da5cff8b8130405adfbff0c52037854235dd
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-02 18:01:49 +0200

    add error handling for async request in deleteInvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -63,6 +63,10 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath("/dashboard/invoices");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+  } catch (error) {
+    return { message: "Database Error: Failed to Delete Invoice." };
+  }
 }
